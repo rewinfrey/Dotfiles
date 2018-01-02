3fcf62e9a716ccfca3d817225673521d0cf0d545
Rick Winfrey <rewinfrey@github.com>
2018-01-01 16:18:21 -0800

    Update setup

--- setup.rb
+++ setup.rb
@@ -2,13 +2,10 @@
 
 # Setup symlinks
 `
-ln -s dotfiles/emacs_files/             .emacs.d;
 ln -s dotfiles/bash_files/.bash_profile .bash_profile;
-ln -s dotfiles/zsh_files/.zshrc         .zshrc;
 ln -s dotfiles/vim_files                .vim;
 ln -s dotfiles/vim_files/.vimrc         .vimrc;
 ln -s dotfiles/tmux_files/.tmux.conf    .tmux.conf;
-ln -s dotfiles/tmux_files/.teamocil     .teamocil;
 ln -s dotfiles/git_files/.gitconfig     .gitconfig;
 ln -s dotfiles/git_files/.gitignore     .gitignore
 `
