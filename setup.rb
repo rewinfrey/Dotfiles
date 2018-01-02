77edebd1206f86d220850ae51bdfb676dc3562d6
Rick Winfrey <rewinfrey@github.com>
2018-01-01 16:06:50 -0800

    Add to setup

--- setup.rb
+++ setup.rb
@@ -13,9 +13,19 @@ ln -s dotfiles/git_files/.gitconfig     .gitconfig;
 ln -s dotfiles/git_files/.gitignore     .gitignore
 `
 
-# Bootstrap basic environment
-`
-git clone https://github.com/mikemcquaid/strap
-cd strap
-bin/strap.sh --debug
-`
+# Brewfile
+`brew bundle`
+
+# System configuration
+
+# Initial key repeat (default is 15 (225 ms))
+`defaults write -g InitialKeyRepeat -int 15`
+
+# Faster key repeat (default is 2 (30 ms))
+`defaults write -g KeyRepeat -int 1`
+
+# Disable Accent Menu when you hold down a key
+`defaults write -g ApplePressAndHoldEnabled -bool false`
+
+# Hide desktop icons
+`defaults write com.apple.finder CreateDesktop false; killall Finder`
